fa79291477f5c1d8be101ba6870a0be2244c0ecc
Deepak Karn <deepak.karn@riskcovry.com>
2023-08-07 21:02:56 +0530

    adding add product and get Product

--- src/Components/AddProduct/AddProduct.jsx
+++ src/Components/AddProduct/AddProduct.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./AddProduct.css"
+import axios from "axios";
 
 function AddProduct() {
+    const [objData, setObjData] = useState({})
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post("http://localhost:3001/products", objData)
+            .then(({ data }) => {
+                console.log(data)
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    }
+
+
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+
+        setObjData({ ...objData, [name]: value })
+    }
+
     return (
-        <div>AddProduct</div>
+        <center>
+            <form className='addProductMainContainer' onSubmit={handleSubmit}>
+                <input type="text" name='name' placeholder='Product Name' onChange={handleChange} />
+                <input type="text" name='description' placeholder='Enter Description' onChange={handleChange} />
+
+
+                <select name="category" placeholder='Select Category' onChange={handleChange}>
+                    <option value="">Select Category</option>
+                    <option value="clothing">Clothing</option>
+                    <option value="electronics">Electronics</option>
+                    <option value="furniture">Furniture</option>
+                </select>
+
+                <input type="text" name='image' placeholder='Enter Image Url' onChange={handleChange} />
+                <input type="text" name='location' placeholder='Enter Location' onChange={handleChange} />
+                <input type="date" name='postedAt' placeholder='Select Date' onChange={handleChange} />
+                <input type="number" name='price' placeholder='Enter Price' onChange={handleChange} />
+
+                <input type="submit" />
+            </form>
+
+        </center>
     )
 }
 
--- src/Components/GetProduct/GetProduct.jsx
+++ src/Components/GetProduct/GetProduct.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-import "./GetProduct.css"
+import React, { useEffect, useState } from 'react'
+import "./GetProduct.css";
+import axios from "axios"
 
 function GetProduct() {
+
+    const [productData, setProductData] = useState([])
+
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+
+    const fetchData = () => {
+        axios.get("http://localhost:3001/products")
+            .then(({ data }) => {
+                setProductData(data)
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    }
+
+
     return (
-        <div>GetProduct</div>
+        <div>
+            {productData.map((elem) => {
+                return (
+                    <div key={elem.id} className='mainContainer'>
+                        <div className='imgContainer'>
+                            <img src={elem.img} alt="product_img" />
+                        </div>
+                        <div className='Title'> {elem.name}</div>
+                        <div className='title'>{elem.category}</div>
+                        <div className='title'>{elem.description} </div>
+                        <div className='title'>Location : {elem.location}</div>
+                        <div className='title'>Posted On : {elem.postedAt}</div>
+                        <div className='Title'>Rs . {elem.price} /-</div>
+                        <button>Buy</button>
+
+                    </div>
+                )
+            })}
+
+        </div>
     )
 }
 
